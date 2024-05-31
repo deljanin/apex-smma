8a08f5661b6dbb1e72f3451a184e2322fc3fd887
Petar <pecadeljanin@gmail.com>
2024-05-31 21:57:22 +0200

    Add Vercel analytics.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { inject } from '@vercel/analytics';
 	injectSpeedInsights();
+	inject();
 
 	import { slide } from 'svelte/transition';
 	// import { onMount } from 'svelte';
