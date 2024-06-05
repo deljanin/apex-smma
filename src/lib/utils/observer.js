17da9405224b6a09c594b143ea61ea0a12373ff6
deljanin <pecadeljanin@gmail.com>
2024-06-06 00:40:03 +0200

    Change intersection observer to dispatch events when objects are fully visible.

--- src/lib/utils/observer.js
+++ src/lib/utils/observer.js
@@ -3,12 +3,15 @@ let intersectionObserver;
 function useIntersectionObserver() {
 	if (intersectionObserver) return;
 
-	intersectionObserver = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			const eventName = entry.isIntersecting ? 'enterViewport' : 'exitViewport';
-			entry.target.dispatchEvent(new CustomEvent(eventName));
-		});
-	});
+	intersectionObserver = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				const eventName = entry.isIntersecting ? 'enterViewport' : 'exitViewport';
+				entry.target.dispatchEvent(new CustomEvent(eventName));
+			});
+		},
+		{ threshold: 1 }
+	);
 }
 
 export default function viewport(element) {
